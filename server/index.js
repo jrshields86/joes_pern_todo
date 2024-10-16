fe719c7890d5705970f67adb8efc155d5c390395
jrshields86 <jrshields86@gmail.com>
2024-10-16 12:44:47 -0600

    get all and get specific routes in place

--- server/index.js
+++ server/index.js
@@ -14,10 +14,10 @@ app.use(express.json());  //req.body
 app.post("/todos", async(req, res) => {
     try {
         const { description } = req.body;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)",
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING * ",
             [description]
         );
-        res.json(newTodo);
+        res.json(newTodo.rows[0]);
     } catch (error) {
         console.error(error.message);
     }
@@ -25,7 +25,27 @@ app.post("/todos", async(req, res) => {
 
 //get all todos
 
+app.get("/todos", async(req,res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
 //get a todo
+
+app.get("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+
+        res.json(todo.rows[0]);
+    } catch (error) {           
+        console.error(error.message);        
+    }
+});
 
 //update a todo
 
