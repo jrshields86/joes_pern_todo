e7279e35be234365f88d37b21768618985597b02
jrshields86 <jrshields86@gmail.com>
2024-10-19 17:27:12 -0600

    Working on DeleteTodo button

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -4,6 +4,19 @@ const ListTodos = () => {
 
     const [todos, setTodos] = useState([]);
 
+    //delete todo function
+
+    const deleteTodo = async(id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            }); 
+
+            console.log(deleteTodo);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
 
     const getTodos = async () => {
         try {
@@ -42,10 +55,16 @@ const ListTodos = () => {
                     <td>john@example.com</td>
                 </tr> */}
                 {todos.map(todo => (
-                        <tr>
+                        <tr key={todo.todo_id}>
                             <td>{todo.description}</td>
                             <td>Edit</td>
-                            <td>Delete</td>
+                            <td><button
+                                    className="btn btn-danger"
+                                    onClick={() => deleteTodo(todo.todo_id)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
