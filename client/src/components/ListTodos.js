7ae83dd55574bacf046561f7c712398041c43e8e
jrshields86 <jrshields86@gmail.com>
2024-10-28 16:22:19 -0600

    adding modal code

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect, useState } from "react";
+import EditTodo from "./EditTodo";
 
 const ListTodos = () => {
 
@@ -56,7 +57,9 @@ const ListTodos = () => {
                 {todos.map(todo => (
                         <tr key={todo.todo_id}>
                             <td>{todo.description}</td>
-                            <td>Edit</td>
+                            <td>
+                                <EditTodo />
+                            </td>
                             <td><button
                                     className="btn btn-danger"
                                     onClick={() => deleteTodo(todo.todo_id)}
