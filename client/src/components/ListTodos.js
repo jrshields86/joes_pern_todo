60287130513733592c3db5c175642ff5f6ae9fe4
jrshields86 <jrshields86@gmail.com>
2024-10-29 16:03:54 -0600

    edit component progress

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -58,7 +58,7 @@ const ListTodos = () => {
                         <tr key={todo.todo_id}>
                             <td>{todo.description}</td>
                             <td>
-                                <EditTodo />
+                                <EditTodo todo={todo} />
                             </td>
                             <td><button
                                     className="btn btn-danger"
