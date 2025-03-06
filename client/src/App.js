2ab48796b002e53622c53ba7e8380802a58a65cd
jrshields86 <jrshields86@gmail.com>
2025-03-06 16:05:26 -0700

    more client side refactoring and login work

--- client/src/App.js
+++ client/src/App.js
@@ -12,32 +12,33 @@ import { useState } from 'react';
 function App() {
   const [auth, setAuth] = useState({});
 
-  useEffect(()=> {
-    const attemptLoginWithToken = async()=> {
-      const token = window.localStorage.getItem('token');
-        if(token){
+  const attemptLoginWithToken = async()=> {
+    const token = window.localStorage.getItem('token');
+      if(token){
+        try {
           const response = await axios.get('https://joes-pern-todo-backend.onrender.com/me', {
             headers: {
               authorization: token
             }
       });
       setAuth(response.data);
-    }
-    };
+        } catch (ex) {
+          if(ex.response.status === 401){
+            window.localStorage.removeItem('token');
+          }
+          }
+  }
+  };
+
+  useEffect(()=> {
     attemptLoginWithToken();
   }, []);
 
   const login = async(credentials)=> {
-    let response = await axios.post("https://joes-pern-todo-backend.onrender.com/login", credentials);
+    const response = await axios.post("https://joes-pern-todo-backend.onrender.com/login", credentials);
     const { token } = response.data;
     window.localStorage.setItem('token', token);
-
-    response = await axios.get('https://joes-pern-todo-backend.onrender.com/me', {
-      headers: {
-        authorization: token
-      }
-    });
-    setAuth(response.data);
+    attemptLoginWithToken();
   };
 
   return (
